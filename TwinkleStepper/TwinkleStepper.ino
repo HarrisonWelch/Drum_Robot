4683e46cbcb6585592f7d7f4c49fb327bb6aecf0
Harrison <harrison@localhost.localdomain>
2017-05-08 18:03:59 -0400

    twinkle little star written, testing code commented out, renamed functions, left some in-line comments

--- TwinkleStepper/TwinkleStepper.ino
+++ TwinkleStepper/TwinkleStepper.ino
@@ -2,7 +2,7 @@
  Name:		TwinkeStepper.ino
  Created:	1/3/2017 5:37:27 PM
  Author:	HARRISON WELCH
- Last Edit: 03-01-2017 3:06:46 PM
+ Last Edit: 04-08-2017 4:04:00 PM
 */
 
 //HardwareSerial Serial;
@@ -14,6 +14,9 @@
 
 char val;
 const int ONE_HUNDRED = 100;
+const int std_delay_time = 500;//ms
+const int std_step_amt = 20;//steps
+
 
 Adafruit_MotorShield top(0x60);//init top    shield
 Adafruit_MotorShield bot(0x61);//init bottom shield
@@ -33,16 +36,56 @@ void setup() {
 
 // the loop function runs over and over again until power down or reset
 void loop() {
+  
+  /*Twinkle-Twinkle Little Star
+   * GGDDEED
+   * CCBBAAG
+   * DDCCBBA
+   * DDCCBBA
+   * GGDDEED
+   * CCBBAAG
+   * 
+   * Translate to my numbered functions
+   * 
+   * 7744554
+   * 3322117
+   * 4433221
+   * 4433221
+   * 7744554
+   * 3322117
+   */
 
-	playF();
-	playG();
-	playA();
-	playB();
-	playC();
-	playD();
-	playE();
-	playF2();
-	delay(1000);
+  play7();play7();play4();play4();play5();play5();play4();delay(500);
+  play3();play3();play2();play2();play1();play1();play7();delay(500);
+  play4();play4();play3();play3();play2();play2();play1();delay(500);
+  play4();play4();play3();play3();play2();play2();play1();delay(500);
+  play7();play7();play4();play4();play5();play5();play4();delay(500);
+  play3();play3();play2();play2();play1();play1();play7();delay(500);
+   
+  //simple up-scale//
+  /*
+  play1();//6
+  play2();//5
+  play3();//1
+  play4();//2
+  play5();//3
+  play6();//4
+  play7();//8
+  play8();//7 
+  */
+  /*
+  //old code from be
+	playF();//1
+	playG();//2
+	playA();//3
+	playB();//4
+	playC();//5
+	playD();//6
+	playE();//7
+	playF2();//8
+	*/
+  
+	delay(1000); //testing line
 
 }
 
@@ -75,36 +118,38 @@ void play(Adafruit_StepperMotor *stepper, int steps, int dir) {
 	delay(100);
 }
 void play(Adafruit_StepperMotor *stepper, int dir) {
-	stepper->step(15, dir, DOUBLE);
-	delay(ONE_HUNDRED);
+	stepper->step(std_step_amt, dir, DOUBLE);
+	delay(std_delay_time);
 	if (dir == 1) { dir = 2; }
 	else { dir = 1; }
-	stepper->step(15, dir, DOUBLE);
-	delay(ONE_HUNDRED);
+	stepper->step(std_step_amt, dir, DOUBLE);
+	delay(std_delay_time);
 }
 
-void playF() {
+//TODO: sort this list of functions
+
+void play3() {
 	play(Stepper4, BACKWARD);
 }
-void playG() {
+void play4() {
 	play(Stepper4, FORWARD);
 }
-void playA() {
+void play5() {
 	play(Stepper3, BACKWARD);
 }
-void playB() {
+void play6() {
 	play(Stepper3, FORWARD);
 }
-void playC() {
+void play2() {
 	play(Stepper2, BACKWARD);
 }
-void playD() {
+void play1() {
 	play(Stepper2, FORWARD);
 }
-void playE() {
+void play8() {
 	play(Stepper1, BACKWARD);
 }
-void playF2() {
+void play7() {
 	play(Stepper1, FORWARD);
 }
 void playG2() {}
